8cf30d4c5908b226d54f83de9e9b0082b04c7bdf
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-04 06:12:03 +0200

    initial version

---
+++ sat-cookie-decorator.js
@@ -0,0 +1,19 @@
+angular.module('satellizer')
+.requires.push('ngCookies');
+
+angular.module('satellizer')
+.decorator('SatellizerStorage', function($delegate, $cookies) {
+  var storage = {
+    get: function(key) {
+      return $cookies.get(key);
+    },
+    set: function(key, value) {
+      $cookies.put(key, value, { path: '/', domain: '.guclink.in', secure: true });
+    },
+    remove: function(key) {
+      $cookies.remove(key);
+    }
+  };
+
+  return storage;
+});
