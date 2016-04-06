517a1e83cdb282bcdc239324436b1ff408573ceb
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-06 19:12:06 +0200

    changed dependency to angular-cookies from ng-cookie

--- sat-cookie-decorator.js
+++ sat-cookie-decorator.js
@@ -1,5 +1,5 @@
 angular.module('satellizer')
-.requires.push('ngCookie');
+.requires.push('ngCookies');
 
 angular.module('satellizer')
 .decorator('SatellizerStorage', function($delegate, $cookies) {
