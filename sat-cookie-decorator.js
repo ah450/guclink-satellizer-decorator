2d4d53970457f11f7aa7532c5fee705a323413d7
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-15 17:55:05 +0200

    changes cookiesProvider defaults

--- sat-cookie-decorator.js
+++ sat-cookie-decorator.js
@@ -2,13 +2,21 @@ angular.module('satellizer')
 .requires.push('ngCookies');
 
 angular.module('satellizer')
+  .config (['$cookiesProvider', function($cookiesProvider) {
+    $cookiesProvider.defaults =
+      path: '/'
+      domain: '.guclink.in'
+      secure: true
+  }]);
+
+angular.module('satellizer')
 .decorator('SatellizerStorage', ['$delegate', '$cookies', function($delegate, $cookies) {
   var storage = {
     get: function(key) {
       return $cookies.get(key);
     },
     set: function(key, value) {
-      $cookies.put(key, value, { path: '/', domain: '.guclink.in', secure: true });
+      $cookies.put(key, value);
     },
     remove: function(key) {
       $cookies.remove(key);
