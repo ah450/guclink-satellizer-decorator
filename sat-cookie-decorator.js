cf6697e465abc5799f43ceb551e06ea8bd20c4aa
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-15 18:13:36 +0200

    adds logging

--- sat-cookie-decorator.js
+++ sat-cookie-decorator.js
@@ -4,22 +4,25 @@ angular.module('satellizer')
 angular.module('satellizer')
   .config (['$cookiesProvider', function($cookiesProvider) {
     $cookiesProvider.defaults = {
-      path: '/'
-      domain: '.guclink.in'
+      path: '/',
+      domain: '.guclink.in',
       secure: true
     }
   }]);
 
 angular.module('satellizer')
-.decorator('SatellizerStorage', ['$delegate', '$cookies', function($delegate, $cookies) {
+.decorator('SatellizerStorage', ['$cookies', function($cookies) {
   var storage = {
     get: function(key) {
+      console.log('get', key)
       return $cookies.get(key);
     },
     set: function(key, value) {
+      console.log('put', key, value)
       $cookies.put(ke y, value);
     },
     remove: function(key) {
+      console.log('remove', key)
       $cookies.remove(key);
     }
   };
